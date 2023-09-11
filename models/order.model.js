09096b8274e2f1467286487cad832ef6e7056409
Zainul Karim <achmadzainulkarim@gmail.com>
2023-09-11 22:30:04 +0700

    init model

---
+++ models/order.model.js
@@ -0,0 +1,6 @@
+import Model from './model.js';
+
+export default class OrderModel extends Model{
+
+}
+
---
+++ models/product.model.js
@@ -0,0 +1,6 @@
+import Model from './model.js';
+
+export default class ProductModel extends Model{
+
+}
+
